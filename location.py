243272755e3b3f9626373bf618a4d2dac11800db
Arjun Mehta <arjujmehta23@gmail.com>
2023-06-11 14:54:19 -0700

    Add 3D get_location, refactor code

--- location.py
+++ location.py
@@ -1,40 +1,63 @@
 # Refer to https://math.stackexchange.com/questions/1725790/calculate-third-point-of-triangle-from-two-points-and-angles
 
-import math
+from model.point import Point
+from utils.constants import Mode
 
-def get_tag_location(beamer_angles, beamer_positions):
+import math
+from typing import List
+
+"""
+Computes the location of the tag
+
+beamer_angles       : list of angles of incidence of beamers (degrees)
+beamer_positions    : list of positions of beamers (List[Point])
+"""
+def get_tag_location(beamer_angles: List[float], beamer_positions: List[Point], mode: Mode = Mode.TWO_D):
   
   # Check if data is valid
   if len(beamer_angles) < 2:
     print("Data from atleast 2 beamers required. Aborting...")
     return
+    
+  # Extract angles of known beamers
+  angle_b1 = beamer_angles[0]
+  angle_b2 = beamer_angles[1]
+  
+  # Extract positions of known beamers
+  pos_b1: Point = beamer_positions[0]
+  pos_b2: Point = beamer_positions[1]
+  
+  # Compute the two possible solutions 
+  res1 = compute_xy_coordinates(angle_b1, angle_b2, pos_b1, pos_b2)
+  res2 = compute_xy_coordinates(angle_b2, angle_b1, pos_b2, pos_b1)
 
-  # 2D location
-  elif len(beamer_angles) == 2:
-    
-    # Extract angles of known beamers
-    angle_b1 = beamer_angles[0]
-    angle_b2 = beamer_angles[1]
-    
-    # Extract positions of known beamers
-    pos_b1 = beamer_positions[0]
-    pos_b2 = beamer_positions[1]
-    
-    # Compute the two possible solutions 
-    res1 = compute_2d_coordinates(angle_b1, angle_b2, pos_b1, pos_b2)
-    res2 = compute_2d_coordinates(angle_b2, angle_b1, pos_b2, pos_b1)
-    
+  # res = eliminate(res1, res2) TODO: implement error correction
+  # We are getting two (or more) possible solutions.
+  # We need to find the most likely correct solution.
+
+  if mode == Mode.TWO_D:
     return [res1, res2]
-    
-  # 3D location
-  else:
-    print("TODO")
+  
+  if mode == Mode.THREE_D:
+    pos_b3 = beamer_positions[2] # TODO: right now, we only have 3 beamers. 1 and 2 are on one plane, 3 is on another.
+    z1 = calculate_z_coordinate(res1, pos_b3, angle_b1, True)
+    z2 = calculate_z_coordinate(res2, pos_b3, angle_b1, True)
 
-def compute_2d_coordinates(angle_b1, angle_b2, pos_b1, pos_b2):
+    return [Point(res1[0], res1[1], z1), Point(res2[0], res2[1], z2)]
+
+"""
+Computes x and y coordinates of tag
+
+angle_b1  : incidence angle of beamer 1 (degrees)
+angle_b2  : incidence angle of beamer 2 (degrees)
+pos_b1    : position of beamer 1
+pos_b2    : position of beamer 2
+"""
+def compute_xy_coordinates(angle_b1, angle_b2, pos_b1: Point, pos_b2: Point):
   
   # Extract coordinates of known points
-  x1, y1 = pos_b1
-  x2, y2 = pos_b2
+  x1, y1, _ = pos_b1.get()
+  x2, y2, _ = pos_b2.get()
   
   # Compute x and y offset
   u = x2 - x1
@@ -50,11 +73,57 @@ def compute_2d_coordinates(angle_b1, angle_b2, pos_b1, pos_b2):
   
   # Set up equations
   l = distance * (math.sin(a2) / math.sin(a3))
-  RHS1 = (x1 * u) + (y1 * v) + (l * distance * math.cos(a1))
-  RHS2 = (y2 * u) - (x2 * v) - (l * distance * math.sin(a1))
+  eq1 = (x1 * u) + (y1 * v) + (l * distance * math.cos(a1))
+  eq2 = (y2 * u) - (x2 * v) - (l * distance * math.sin(a1))
   
   # Calculate coordinates of third point
-  x3 = (1 / distance**2) * ((u * RHS1) - (v * RHS2))
-  y3 = (1 / distance**2) * ((v * RHS1) + (u * RHS2))
+  x3 = (1 / distance**2) * ((u * eq1) - (v * eq2))
+  y3 = (1 / distance**2) * ((v * eq1) + (u * eq2))
   
   return (x3, y3)
+
+"""
+Calculates z-coordinate of tag
+
+tag_coordinates     : coordinates of tag
+beamer_coordinates  : coordinates of beamer
+angle               : angle of inclination/declination of tag wrt beamer (degrees)
+is_tag_below        : True if tag is below beamer, False otherwise
+"""
+def calculate_z_coordinate(tag_coordinates: Point, beamer_coordinates: Point, angle: float, is_tag_below: bool):
+  # Extract coordinates of tag
+  x, y, _ = tag_coordinates.get()
+
+  # Extract coordinates of beamer
+  x_b, y_b, z_b = beamer_coordinates.get()
+
+  # Convert angle to radians
+  a = math.radians(angle)
+
+  # Calculate x and y offset
+  u = x_b - x
+  v = y_b - y
+
+  # Calculate xy offset
+  d = math.sqrt(u**2 + v**2)
+
+  # Calculate perpendicular
+  p = math.tan(a) * d
+
+  # Calculate z offset
+  if is_tag_below:
+    ans = z_b - p
+  else:
+    ans = z_b + p
+
+  return ans
+
+## Testing
+
+## Test get_tag_location() for 2D
+# print(get_tag_location([30, 30], [Point(5,2), Point(2,3)]))
+
+## Test calculate_z_coordinate()
+# tag_coordinate_1 = Point(0, 1)
+# beamer_coordinate_1 = Point(3, 4, 10)
+# print(calculate_z_coordinate(tag_coordinate_1, beamer_coordinate_1, 30, True))
---
+++ utils/constants.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class Mode(Enum):
+  TWO_D = 1
+  THREE_D = 2
