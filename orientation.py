0552b4477e785c637ec0328cf034226777ee95a4
Arjun Mehta <arjujmehta23@gmail.com>
2023-06-16 18:16:25 -0700

    set up variables in orientation.py

--- orientation.py
+++ orientation.py
@@ -1,18 +1,19 @@
 from model.point import Point
 
 from typing import List
+import numpy as np
 
 
 def get_orientation(
     m_powers: List[List[float]],
-    m_tag_vectors: List[List[Point]],
+    m_tag_vectors: List[List[float]],
     m_normalized_distances: List[List[float]],
     m_intensities: List[List[float]],
 ):
     """
     Computes the orientation of the tag
 
-    Each outer list represents data from multiple tag locations
+    Outer lists represent data from multiple tag locations
     m_powers                  : each inner list contains powers of each light source
     m_tag_vectors             : each inner list contains vectors from tag to each light source
     m_normalized_distances    : each inner list contains normalized distances between tag and each light source
@@ -57,11 +58,29 @@ def process_position_data(
         print("Error: Invalid data. All input lists must be of the same size.")
         return
 
-    num_light_sources = len(powers)
-    q_list: List[float] = []
-    k = 1
+    if len(powers) < 3:
+        print("Error: Data from atleast 3 light sources required.")
+        return
 
+    # Create variables for list of q values and unknown gain k
+    q = []
+    k = 1  # TODO: This value is a placeholder. We need to account for it in the equations.
+
+    # populate list of q values
+    num_light_sources = len(powers)
     for i in range(num_light_sources):
-        q_list.append((intensities[i] * normalized_distances[i] ** 2) / (k * powers[i]))
+        q.append((intensities[i] * normalized_distances[i] ** 2) / (k * powers[i]))
+
+    # Create numpy array for q values and its transpose
+    b = np.array(q)
+    b_t = np.transpose(b)
+
+    # Create numpy array for tag vectors, its pseudo-inverse and its pseudo-inverse-transpose
+    v = np.array(tag_vectors)
+    v_pi = np.linalg.pinv(v)
+    v_pi_t = np.transpose(v_pi)
+
+    # Set c as the matrix multiplication of the pseudo-inverse and its transpose
+    c = np.matmul(v_pi, v_pi_t)
 
     # TODO: To be continued...
