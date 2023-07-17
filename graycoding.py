9cc9df0ff13e724ddb2ac39be99b5acf91910d36
Aryaman Singh <aryaman.singh.2012@gmail.com>
2023-07-17 16:07:24 -0400

    graycoding fix

--- graycoding.py
+++ graycoding.py
@@ -1,12 +1,12 @@
 from typing import List
 
 # TODO: Recursion is (maybe) not the best way to do this?
-def gray_code(bits: int) -> List[int]:
+def gray_code(bits: int) -> List[str]:
     if bits == 1:
-        return [0, 1]
+        return ["0", "1"]
     else:
-        prev = gray_code(bits - 1)
-        return [0 + i for i in prev] + [1 + i for i in prev[::-1]]
+        codes = gray_code(bits - 1)
+        return ["0" + code for code in codes] + ["1" + code for code in codes[::-1]]
 
 # Test gray_code()
 print(gray_code(8))
